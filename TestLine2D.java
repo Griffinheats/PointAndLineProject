e0ff137ba6bb4dee1cac599ecab88ebaead51aee
Griffinheats <99288944+Griffinheats@users.noreply.github.com>
2023-01-20 08:58:15 -0500

    .

--- TestLine2D.java
+++ TestLine2D.java
@@ -11,16 +11,21 @@ public class TestLine2D
         Line2D line2 = new Line2D(end3, end4);
         Line2D line3 = new Line2D(4, 9, 6, 8);
 
-        System.out.println("get");
-        System.out.println(line1.getP1());
-        System.out.println(line1.getP2());
-        System.out.println(line2.getP1());
-        System.out.println(line2.getP2());
-        System.out.println("slope");
-        System.out.println(line1.getSlope());
-        System.out.println(line2.getSlope());
-        System.out.println("linear");
-        System.out.println(line1.isCollinear(end3));
-        System.out.println(line1.isCollinear(end4));
+        System.out.println("Testing constructor using 2 Point objects and the toString method ");
+        System.out.println("Line 1: " + line1.toString());
+        System.out.println("Line 2: " + line2.toString());
+        System.out.println("Testing constructor using 4 int's and the toString method ");
+        System.out.println("Line 3: " + line3.toString());
+        System.out.println("Testing getP1 and getP2 methods: ");
+        System.out.println("Line 1, getP1: " + line1.getP1());
+        System.out.println("Line 1, getP2: "+ line1.getP2());
+        System.out.println("Line 2, getP1: "+ line2.getP1());
+        System.out.println("Line 2, getP2: " + line2.getP2());
+        System.out.println("Testing getSlope");
+        System.out.println("Line 1 slope: " + line1.getSlope());
+        System.out.println("Line 2 slope: " + line2.getSlope());
+        System.out.println("Testing isCollinear");
+        System.out.println("Line1 and p3: " + line1.isCollinear(end3));
+        System.out.println("Line1 and p4: " + line1.isCollinear(end4));
     }
 }
--- TestPoint.java
+++ TestPoint.java
@@ -9,13 +9,13 @@ public class TestPoint
         Point test5 = new Point(3, 10);
         Point test6 = new Point(6, 8);
 
-        System.out.println("distance");
-        System.out.println(test2.manhattanDistance(test3));
-        System.out.println(test3.manhattanDistance(test4));
-        System.out.println("linear");
-        System.out.println(test2.isCollinear(test4, test5));
-        System.out.println(test2.isCollinear(test3, test4));
-        System.out.println(test1.isCollinear(test2, test6));
-        System.out.println(test1.isCollinear(test2, test3));
+        System.out.println("Testing manhattanDistance:");
+        System.out.println("p2 to p3: " + test2.manhattanDistance(test3));
+        System.out.println("p3 to p4: " + test3.manhattanDistance(test4));
+        System.out.println("Testing isCollinear:");
+        System.out.println("p2, p4, p5: " + test2.isCollinear(test4, test5));
+        System.out.println("p2, p3, p4: " + test2.isCollinear(test3, test4));
+        System.out.println("p1, p2, p6: " + test1.isCollinear(test2, test6));
+        System.out.println("p1, p2, p3: " + test1.isCollinear(test2, test3));
     }
 }
